5e40b745d59631ebb8773ebf64035939746a4bf0
chanjunren <chanjunren@gmail.com>
2021-01-14 17:58:43 +0800

    Added navigation component, added skeleton for navigation component

---
+++ app/src/main/java/com/robosolutions/temiannouncer/HomeFragment.java
@@ -0,0 +1,51 @@
+package com.robosolutions.temiannouncer;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeFragment extends Fragment implements View.OnClickListener {
+    NavController navController;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+
+        view.findViewById(R.id.taskPageButton).setOnClickListener(this);
+        view.findViewById(R.id.configurationsPageButton).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.taskPageButton: {
+                navController.navigate(R.id.action_homeFragment_to_newTaskFragment);
+                break;
+            }
+            case R.id.configurationsPageButton: {
+                navController.navigate(R.id.action_homeFragment_to_configurationFragment);
+                break;
+            }
+        }
+    }
+}
--- app/src/main/java/com/robosolutions/temiannouncer/MainActivity.java
+++ app/src/main/java/com/robosolutions/temiannouncer/MainActivity.java
@@ -1,11 +1,12 @@
 package com.robosolutions.temiannouncer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
